98591980f63a39087dc3aa4917ffaec897544d68
Sirish Gambhira <sgambhira6@atl1-1-01-005-13-0.pace.gatech.edu>
2025-01-18 19:55:24 -0500

    init commit. inference is generating random output

---
+++ inference.py
@@ -0,0 +1,160 @@
+from typing import Optional
+import torch
+import time
+import json
+from pathlib import Path
+from sentencepiece import SentencePieceProcessor
+from model import ModelArgs, Transformer
+from tqdm import tqdm
+import os
+import warnings
+
+class Llama:
+    def __init__(self, model: Transformer, tokenizer: SentencePieceProcessor, model_args: ModelArgs):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.args = model_args
+    
+    @staticmethod
+    def build(checkpoint_dir: str, tokenizer_path: str, load_model: bool, max_seq_len: int, max_batch_size: int, device: str):
+        prev_time = time.time()
+        if load_model:
+            checkpoints = sorted(Path(checkpoint_dir).glob('*.pth'))
+            assert len(checkpoints) > 0, "No checkpoints found"
+            checkpoint = torch.load(checkpoints[0], map_location=device)
+            print(f"Time taken to load model: {(time.time() - prev_time):.2f}s")
+            prev_time = time.time()
+            
+        with open(Path(checkpoint_dir) / 'params.json', 'r') as f:
+            params = json.loads(f.read())
+        
+        model_args = ModelArgs(
+            max_seq_len = max_seq_len,
+            max_batch_size = max_batch_size,
+            device=device,
+            **params
+        )
+        
+        tokenizer = SentencePieceProcessor()
+        tokenizer.load(tokenizer_path)
+        model_args.vocab_size = tokenizer.vocab_size()
+        
+        if device == "cuda":
+            torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        else:
+            torch.set_default_tensor_type(torch.BFloat16Tensor)
+        
+        model = Transformer(model_args).to(device)
+        
+        if load_model:
+            del checkpoint['rope.freqs']
+            model.load_state_dict(checkpoint, strict=True)
+            print(f"Time taken to load checkpoints: {(time.time() - prev_time):.2f}s")
+        
+        return Llama(model, tokenizer, model_args)
+    
+    def generate(self, prompts: list[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len: Optional[int] = None):
+        
+        if max_gen_len is None:
+            max_gen_len = self.args.max_seq_len - 1
+            
+        # generate tokens for prompts
+        prompt_tokens = [self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=False) for prompt in prompts]
+        
+        batch_size=len(prompts)
+        assert batch_size <= self.args.max_batch_size
+        max_prompt_len = max(len(prompt) for prompt in prompt_tokens)
+        assert max_prompt_len <= self.args.max_seq_len
+        
+        total_len = min(self.args.max_seq_len, max_prompt_len + max_gen_len)
+        
+        pad_id = self.tokenizer.pad_id()
+        tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, device=self.args.device)
+        
+        for k, t in enumerate(prompt_tokens):
+            tokens[k, :len(t)] = torch.tensor(t, dtype=torch.long, device=self.args.device)
+        
+        eos_reached = torch.tensor([False] * batch_size, device=self.args.device)
+        prompt_token_mask = tokens != pad_id # true if token is a prompt token
+        
+        for cur_pos in tqdm(range(1, total_len), desc='Generating tokens'):
+            with torch.no_grad():
+                logits = self.model.forward(tokens[:, cur_pos - 1: cur_pos], cur_pos)
+            if temperature > 0:
+                probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
+                next_token = self.sample_top_p(probs, top_p)
+            else:
+                next_token = torch.argmax(logits[:, -1], dim=-1)
+            
+            next_token = next_token.reshape(-1)
+            next_token = torch.where(prompt_token_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            tokens[:, cur_pos] = next_token
+            
+            eos_reached |= (~prompt_token_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id)
+            if all(eos_reached):
+                break
+        
+        output_tokens = []
+        out_text = []
+        for prompt_index, current_tokens in enumerate(tokens.tolist()):
+            if self.tokenizer.eos_id in current_tokens:
+                eos_idx = current_tokens.index(self.tokenizer.eos_id)
+                current_tokens = current_tokens[:eos_idx]
+            output_tokens.append(current_tokens)
+            out_text.append(self.tokenizer.decode(current_tokens))
+        return (output_tokens, out_text)
+
+    def sample_top_p(self, probs, p):
+        probs_sort, probs_indx = torch.sort(probs, dim=-1, descending=True)
+        probs_sum = torch.cumsum(probs_sort, dim=-1)
+        mask = probs_sum - probs_sort > p
+        probs_sort[mask] = 0.0
+        probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+        next_token = torch.multinomial(probs_sort, num_samples=1)
+        next_token = torch.gather(probs_indx, -1, next_token)
+        return next_token        
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    # suppress warnings
+    warnings.filterwarnings("ignore")
+    
+    prompts = [
+        "Simply put, the theory of relativity states that ",
+        "If Google was an Italian company founded in Milan, it would",
+        # Few shot promt
+        """Translate English to French:
+        
+        sea otter => loutre de mer
+        peppermint => menthe poivrée
+        plush girafe => girafe peluche
+        cheese =>""",
+        # Zero shot prompt
+        """Tell me if the following person is actually Doraemon disguised as human:
+        Name: Umar Jamil
+        Decision: 
+        """
+    ]
+    
+    model = Llama.build(checkpoint_dir=os.environ['LLAMA_DIR'],
+                        tokenizer_path=os.environ['TOKENIZER_PATH'],
+                        load_model=True,
+                        max_batch_size=8,
+                        max_seq_len=512, 
+                        device=device)
+             
+    # Inference code here
+    out_tokens, out_text = model.generate(prompts, temperature=0.6, top_p=0.9, max_gen_len=64)
+    assert len(out_tokens) == len(prompts)
+    for i in range(len(prompts)):
+        print(f"Prompt: {prompts[i]}")
+        print(f"Generated Text: {out_text[i]}")
+        print(f"Generated Tokens: {out_tokens[i]}")
+        print()
+    
+        
+        
+        
---
+++ model.py
@@ -0,0 +1,196 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from dataclasses import dataclass
+from typing import Optional
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+    
+    def _norm(self, x):
+        return x * torch.rsqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + self.eps)
+    
+    def forward(self, x):
+        return self.weight * self._norm(x).type_as(x)
+
+@dataclass
+class ModelArgs:
+    dim: int = 4096
+    n_layers: int = 32
+    n_heads: int = 32
+    n_kv_heads: int = 32
+    vocab_size: int = -1
+    multiple_of: int = 256
+    ff_dim_multiplier: Optional[float] = None
+    norm_eps: float = 1e-5
+    
+    # KV cache settings
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+    
+    device: str = None
+
+def precompute_theta_pos_freq(d_head : int, seq_len: int, device: str = None, theta: float = 10000.0):
+    assert d_head % 2 == 0, "Head dim must be divisible by 2"
+    theta_exp = torch.arange(0, d_head, 2).float()
+    theta_val = 1 / (theta ** (theta_exp / d_head)).to(device) # [1, d / 2]
+    m = torch.arange(seq_len, device=device).float() # [1, m]
+    freq = torch.outer(m, theta_val) # [m, d / 2]
+    freq_complex = torch.polar(torch.ones_like(freq), freq) # [m, d / 2]
+    return freq_complex
+    
+def apply_rotary_embeddings(x : torch.Tensor, freqs_complex: torch.Tensor, device: str):
+    batch_size, seq_len, n_heads, d_head = x.shape
+    x_grp = x.view(batch_size, seq_len, n_heads, -1, 2) # [b, seq len, d] => [b, seq len, d / 2, 2]
+    x_complex = torch.complex(x_grp[:, :, :, :, 0], x_grp[:, :, :, :, 1]).to(device) # [b, seq len, d / 2]
+    prod = x_complex * freqs_complex.unsqueeze(0) # [b, seq len, d / 2]
+    out = torch.view_as_real(prod) # [b, seq len, d / 2, 2]
+    out = out.reshape(*x.shape).type(torch.float16) # [b, seq len, d]
+    return out
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, args:ModelArgs):
+        super().__init__()
+        hidden_dim = 4 * args.dim
+        hidden_dim = hidden_dim * (2/ 3)
+        if args.ff_dim_multiplier is not None:
+            hidden_dim = int(args.ff_dim_multiplier * hidden_dim)
+        hidden_dim = int(args.multiple_of * math.ceil(hidden_dim / args.multiple_of))
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+    
+    def forward(self, x):
+        swish = F.silu(self.w1(x))
+        x3 = self.w3(x)
+        x = swish * x3
+        return self.w2(x)
+        
+class SelfAttention(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.dim = args.dim
+        self.n_kv_heads = args.n_heads if args.n_kv_heads is None else args.n_kv_heads
+        self.n_heads = args.n_heads
+        self.head_dim = args.dim // args.n_heads
+        self.num_rep = args.n_heads // args.n_kv_heads
+        
+        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, args.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, args.n_kv_heads * self.head_dim, bias=False) # [b, seq len, num kv heads, self.head_dim]
+        self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
+        
+        self.cache_k = torch.zeros((args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim))
+        self.cache_v = torch.zeros((args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim))
+    
+    def forward(self, x, start_pos, freq_complex):
+        batch_size, seq_len, dim = x.shape # (b, 1, dim)
+        assert seq_len == 1, "Sequence length must be 1"
+        
+        xq = self.wq(x) # (B, 1, num q heads * head dim)
+        xk = self.wk(x) # (B, 1, num kv heads * head dim)
+        xv = self.wv(x) # (B, 1, num kv heads * head dim)
+        
+        xq = xq.view(x.shape[0], x.shape[1], self.n_heads, self.head_dim)
+        xk = xk.view(x.shape[0], x.shape[1], self.n_kv_heads, self.head_dim)
+        xv = xv.view(x.shape[0], x.shape[1], self.n_kv_heads, self.head_dim)
+        
+        xq = apply_rotary_embeddings(xq, freq_complex, device=x.device)
+        xk = apply_rotary_embeddings(xk, freq_complex, device=x.device)
+        
+        self.cache_k[:batch_size, start_pos:start_pos+seq_len] = xk
+        self.cache_v[:batch_size, start_pos:start_pos+seq_len] = xv
+        
+        # retrieve all tokens from KV cache
+        keys = self.cache_k[:batch_size, :start_pos+seq_len] # [batch size, seq len, num kv heads, head dim]
+        values = self.cache_v[:batch_size, :start_pos+seq_len]
+        
+        keys = repeat_kv(keys, self.num_rep) # [batch size, seq len, num kv heads * num repeat, head_dim]
+        values = repeat_kv(keys, self.num_rep) # [batch size, seq len, num kv heads * num repeat, head_dim]
+
+        # remove head dim - each head will observe all sequence but only part of the embedding
+        xq = xq.transpose(1, 2) # [batch size, n_heads, seq len, head_dim]
+        keys = keys.transpose(1, 2) #[batch size, n_heads, seq len, head dim]
+        values = values.transpose(1, 2)
+        
+        scores = xq @ keys.transpose(-2, -1) / math.sqrt(self.head_dim)
+        scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+        output = scores @ values # [batch, num q heads, seq len, head_dim]
+        output = output.transpose(1, 2).contiguous().view(batch_size, seq_len, -1) # [batch, seq len, n_heads * head_dim]
+        output = self.wo(output) # [batch, seq len, self.dim]
+        return output
+
+def repeat_kv(x, num_repeat):
+    batch_size, seq_len, n_kv_heads, head_dim = x.shape
+    if num_repeat == 1:
+        return x
+    else:
+        return (
+            # [B, seq len, num kv heads, head dim]
+            x[:, :, :, None, :]
+            .expand(batch_size, seq_len, n_kv_heads, num_repeat, head_dim)
+            .reshape(x.shape[:2], -1, x.shape[-1])
+            # [B, seq len, n_kv_heads * num_repeast, head_dim]
+        )
+
+class EncoderBlock(nn.Module):
+    
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.args = args
+        self.dim = args.dim
+        self.n_heads = args.n_heads
+        assert args.dim % args.n_heads == 0, "Dimension should be divisible by num heads"
+        self.d_head = self.dim // self.n_heads
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForwardBlock(args)
+        self.attention_norm = RMSNorm(args.dim, args.norm_eps)
+        self.ffn_norm = RMSNorm(args.dim, args.norm_eps)
+    
+    def forward(self, x : torch.Tensor, start_pos: int, freq_complex: torch.Tensor):
+        h = x + self.attention(self.attention_norm(x), start_pos, freq_complex)
+        out = h + self.feed_forward(self.ffn_norm(h))
+        return out
+        
+class Transformer(nn.Module):
+    
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        
+        self.args = args
+        self.dim = args.dim
+        assert args.vocab_size != -1, "Vocab size should be set"
+        self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
+        
+        self.layers = nn.ModuleList()
+        for _ in range(args.n_layers):
+            self.layers.append(EncoderBlock(args))
+        
+        self.norm = RMSNorm(dim=args.dim, eps = args.norm_eps)
+        self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
+        self.freqs_complex = precompute_theta_pos_freq(self.args.dim // self.args.n_heads, self.args.max_seq_len * 2, device = self.args.device)
+
+    
+    def forward(self, tokens, start_pos):
+        # input is one token at a time
+        batch_size, seq_len = tokens.shape
+        assert seq_len == 1, "Only one token processed at a time"
+        
+        h = self.tok_embeddings(tokens)
+        
+        freqs_complex = self.freqs_complex[start_pos: start_pos + seq_len]
+        
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_complex)
+        
+        h = self.norm(h)
+        out = self.output(h).float()
+        return out
+
+
+         
+    
